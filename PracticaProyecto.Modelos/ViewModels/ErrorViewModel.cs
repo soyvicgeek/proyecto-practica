71444143a597f57e578a844b24bb0fe41472ae03
Daniel Flores <vflorestrejo@gmail.com>
2024-02-09 08:41:31 -0600

    Configuracion del proyecto

---
+++ PracticaProyecto.Modelos/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticaProyecto.Modelos.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
