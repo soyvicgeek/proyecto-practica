a8eac9b3783047f174f1a16eb64f42b82c6a70fc
Daniel Flores <vflorestrejo@gmail.com>
2024-04-10 21:01:54 -0600

    Add Modulo Marca Backend

---
+++ PracticaProyecto.AccesoDatos/Repositorio/IRepositorio/IMarcaRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using PracticaProyecto.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaProyecto.AccesoDatos.Repositorio.IRepositorio
+{
+    public interface IMarcaRepositorio : IRepositorio<Marca>
+    {
+        void Actualizar(Marca marca);
+    }
+}
---
+++ PracticaProyecto.AccesoDatos/Repositorio/MarcaRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using PracticaProyecto.AccesoDatos.Repositorio.IRepositorio;
+using PracticaProyecto.Data;
+using PracticaProyecto.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaProyecto.AccesoDatos.Repositorio
+{
+    internal class MarcaRepositorio : Repositorio<Marca>, IMarcaRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MarcaRepositorio(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Actualizar(Marca marca)
+        {
+            var marcaBD = _db.Marcas.FirstOrDefault(b => b.Id == marca.Id);
+            if (marcaBD != null)
+            {
+                marcaBD.Nombre = marca.Nombre;
+                marcaBD.Descripcion = marca.Descripcion;
+                marcaBD.Estado = marca.Estado;
+
+                _db.SaveChanges();
+            }
+        }
+    }
+}
